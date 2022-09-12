fc6f4a58215048163eb13d31f2760397b53a0b63
rvnarvaiza <rvnarvaiza@gmail.com>
2022-09-12 16:19:36 +0800

    rv

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,13 +1,29 @@
 export enum GenderEnum {
-    Male = 'Male',
-    Female = 'Female'
+  Male = 'Male',
+  Female = 'Female',
 }
 export type Employee = {
-    name: string;
-    position: string;
-    gender: GenderEnum
+  name: string
+  position: string
+  gender: GenderEnum
 }
 
-export type EmployeeModel = Employee & { 
-    id: number
+export type EmployeeModel = Employee & {
+  id: number
+}
+
+export type Leave = {
+  leaveType: LeaveEnum
+  creditBalance: number
+}
+
+export enum LeaveEnum {
+  SL = 'Sick Leave',
+  SLP = 'Special Leave',
+  VL = 'Vacation Leave',
+}
+
+export type EmployeeLeave = {
+  employeeId: number
+  leave: Leave[]
 }
