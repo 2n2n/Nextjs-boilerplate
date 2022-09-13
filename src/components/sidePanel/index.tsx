f19c36af312df2ff3aecdd917de5a3a96b23c899
allyncubero <allynjosephcubero@gmail.com>
2022-09-13 09:09:57 +0800

    commit formatting issue

--- src/components/sidePanel/index.tsx
+++ src/components/sidePanel/index.tsx
@@ -1,14 +1,17 @@
-import { CloseOutlined, SyncOutlined } from '@ant-design/icons';
-import { Button, Space  } from 'antd'
+import { CloseOutlined, SyncOutlined } from '@ant-design/icons'
+import { Button, Space } from 'antd'
 
 const SidePanel = () => {
-    return(
-        <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-        <Button type="primary" icon={<SyncOutlined/>} block>Refresh</Button>
-        <Button type="primary" icon={<CloseOutlined/>} block>Close</Button>
+  return (
+    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+      <Button type="primary" icon={<SyncOutlined />} block>
+        Refresh
+      </Button>
+      <Button type="primary" icon={<CloseOutlined />} block>
+        Close
+      </Button>
     </Space>
-    );
+  )
 }
 
 export default SidePanel
-
